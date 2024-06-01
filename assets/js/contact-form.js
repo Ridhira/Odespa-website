8943609a0a1ffbe8422c96dd0958216361efd058
umesh sahu <umeshsahu32@gmail.com>
2024-06-01 12:35:03 +0530

    form with google sheet

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -33,7 +33,6 @@ document.addEventListener("keydown", function (e) {
 
 // open modal function
 const openModal = function () {
-  console.log("Button Click");
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
@@ -42,66 +41,84 @@ openModalBtn.addEventListener("click", openModal);
 openModalBtn1.addEventListener("click", openModal);
 
 // ! USER DETAILS FORM
-let userDetails = document.getElementById("user-details");
+// let userDetails = document.getElementById("user-details");
 
-userDetails.addEventListener("submit", (e) => {
+// userDetails.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   // let userName = document.getElementById("userName");
+//   // let phone = document.getElementById("phone");
+//   // let email = document.getElementById("email");
+//   // let submitBtn = document.getElementById("submit_btn");
+//   // let error = document.getElementById("error-message");
+
+//   error.innerHTML = "";
+
+//   if (phone.value.length !== 10) {
+//     error.innerHTML = "Please enter valid phone number";
+//   } else {
+//     let leadStatus = "New Lead";
+//     let leadSource = "Website";
+//     let firstName = userName.value.split(" ")[0];
+//     let lastName = userName.value.split(" ")[1];
+//     let checkboxVal = "Ode Spa";
+//     let message = "Booking Enquiry";
+
+//     submitBtn.innerHTML = "Saving...";
+
+//     console.log(error);
+
+//     fetch(
+//       `https://script.google.com/macros/s/AKfycbwgCzTPxP13H4LGiRtTGeqAxSOTmLy7t_rfffTm0fKy3CGtyWJlQTBNEhEZ6exXYBNg/exec`,
+//       {
+//         method: "post",
+//       }
+//     )
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .then((data) => {
+//         console.log("Data--->", data);
+//         if (data.data[0].status === "success") {
+//           error.classList.add("success_text");
+//           error.classList.remove("error-text");
+//           error.innerHTML =
+//             "Your form details submitted successfully. Thankyou!! ";
+//           submitBtn.innerHTML = "Submit";
+
+//           // window.location.replace(
+//           //   "https://odespa.zenoti.com/webstoreNew/services?ref=lbb"
+//           // );
+//           window.open(
+//             "https://odespa.zenoti.com/webstoreNew/services?ref=lbb",
+//             "_blank"
+//           );
+//         } else {
+//           console.log("In Else Block");
+//           error.innerHTML = "Something went wrong. Please Try Again!!";
+//           submitBtn.innerHTML = "Submit";
+//         }
+//       })
+//       .catch((error) => {
+//         console.error("Error:", error);
+//       });
+//   }
+// });
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbwgCzTPxP13H4LGiRtTGeqAxSOTmLy7t_rfffTm0fKy3CGtyWJlQTBNEhEZ6exXYBNg/exec";
+
+const form = document.forms["user-form"];
+
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  // let userName = document.getElementById("userName");
-  // let phone = document.getElementById("phone");
-  // let email = document.getElementById("email");
-  // let submitBtn = document.getElementById("submit_btn");
-  // let error = document.getElementById("error-message");
-
-  error.innerHTML = "";
-
-  if (phone.value.length !== 10) {
-    error.innerHTML = "Please enter valid phone number";
-  } else {
-    let leadStatus = "New Lead";
-    let leadSource = "Website";
-    let firstName = userName.value.split(" ")[0];
-    let lastName = userName.value.split(" ")[1];
-    let checkboxVal = "Ode Spa";
-    let message = "Booking Enquiry";
-
-    submitBtn.innerHTML = "Saving...";
-
-    console.log(error);
-
-    fetch(
-      `https://ridhirazen.com/zohocrmleadcreate.php?phone_number=${phone.value}&first_name=${firstName}&last_name=${lastName}&email=${email.value}&product=${checkboxVal}&lead_source=${leadSource}&lead_status=${leadStatus}&description=${message}&mobile=${phone.value}`,
-      {
-        method: "get",
-      }
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) =>
+      alert("Thank you! your form is submitted successfully.")
     )
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log("Data--->", data);
-        if (data.data[0].status === "success") {
-          error.classList.add("success_text");
-          error.classList.remove("error-text");
-          error.innerHTML =
-            "Your form details submitted successfully. Thankyou!! ";
-          submitBtn.innerHTML = "Submit";
-
-          // window.location.replace(
-          //   "https://odespa.zenoti.com/webstoreNew/services?ref=lbb"
-          // );
-          window.open(
-            "https://odespa.zenoti.com/webstoreNew/services?ref=lbb",
-            "_blank"
-          );
-        } else {
-          console.log("In Else Block");
-          error.innerHTML = "Something went wrong. Please Try Again!!";
-          submitBtn.innerHTML = "Submit";
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  }
+    .then(() => {
+      window.location.reload();
+    })
+    .catch((error) => console.error("Error!", error.message));
 });
