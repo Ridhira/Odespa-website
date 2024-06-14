533bef48f6545481f40c87ebc408d27c902ed24d
umesh sahu <umeshsahu32@gmail.com>
2024-06-14 18:15:14 +0530

    in contact form whatsapp api is integrated

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -40,70 +40,35 @@ const openModal = function () {
 openModalBtn.addEventListener("click", openModal);
 openModalBtn1.addEventListener("click", openModal);
 
-// ! USER DETAILS FORM
-// let userDetails = document.getElementById("user-details");
+// @ FORM SUBMISSION
+const fnSendWhatsappNotification = async (name, phone, email) => {
+  var template = {
+    id: "46365bd4-ff0d-45de-aa57-08d348a89583",
+    params: [name, phone, email],
+  };
 
-// userDetails.addEventListener("submit", (e) => {
-//   e.preventDefault();
+  const response = await fetch(
+    "https://api.gupshup.io/wa/api/v1/template/msg",
+    {
+      method: "post",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Cache-Control": "no-cache",
+        apikey: "v5puhrcusaw2hsrc7upudfcs9kzz4ubl",
+      },
+      body: new URLSearchParams({
+        channel: "whatsapp",
+        source: "917995026846",
+        destination: "917207925841", // Send the message to the provided customer number
+        "src.name": "Snogf0uw48PjDbJ44sboh5v5",
+        template: JSON.stringify(template),
+      }),
+    }
+  );
 
-//   // let userName = document.getElementById("userName");
-//   // let phone = document.getElementById("phone");
-//   // let email = document.getElementById("email");
-//   // let submitBtn = document.getElementById("submit_btn");
-//   // let error = document.getElementById("error-message");
-
-//   error.innerHTML = "";
-
-//   if (phone.value.length !== 10) {
-//     error.innerHTML = "Please enter valid phone number";
-//   } else {
-//     let leadStatus = "New Lead";
-//     let leadSource = "Website";
-//     let firstName = userName.value.split(" ")[0];
-//     let lastName = userName.value.split(" ")[1];
-//     let checkboxVal = "Ode Spa";
-//     let message = "Booking Enquiry";
-
-//     submitBtn.innerHTML = "Saving...";
-
-//     console.log(error);
-
-//     fetch(
-//       `https://script.google.com/macros/s/AKfycbwgCzTPxP13H4LGiRtTGeqAxSOTmLy7t_rfffTm0fKy3CGtyWJlQTBNEhEZ6exXYBNg/exec`,
-//       {
-//         method: "post",
-//       }
-//     )
-//       .then((response) => {
-//         return response.json();
-//       })
-//       .then((data) => {
-//         console.log("Data--->", data);
-//         if (data.data[0].status === "success") {
-//           error.classList.add("success_text");
-//           error.classList.remove("error-text");
-//           error.innerHTML =
-//             "Your form details submitted successfully. Thankyou!! ";
-//           submitBtn.innerHTML = "Submit";
-
-//           // window.location.replace(
-//           //   "https://odespa.zenoti.com/webstoreNew/services?ref=lbb"
-//           // );
-//           window.open(
-//             "https://odespa.zenoti.com/webstoreNew/services?ref=lbb",
-//             "_blank"
-//           );
-//         } else {
-//           console.log("In Else Block");
-//           error.innerHTML = "Something went wrong. Please Try Again!!";
-//           submitBtn.innerHTML = "Submit";
-//         }
-//       })
-//       .catch((error) => {
-//         console.error("Error:", error);
-//       });
-//   }
-// });
+  const res = await response.json();
+  console.log("res", res);
+};
 
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbwgCzTPxP13H4LGiRtTGeqAxSOTmLy7t_rfffTm0fKy3CGtyWJlQTBNEhEZ6exXYBNg/exec";
@@ -114,9 +79,14 @@ const submitButton = document.querySelector(".submit-btn");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   submitButton.textContent = "Saving...";
+  const userName = document.getElementById("fullName").value;
+  const userPhone = document.getElementById("phoneNumber").value;
+  const userEmail = document.getElementById("emailAddress").value;
 
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
     .then((response) => {
+      console.log("response", response);
+      fnSendWhatsappNotification(userName, userPhone, userEmail);
       localStorage.setItem("userData", true);
       alert("Thank you! your form is submitted successfully.");
     })
